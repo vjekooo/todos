ccc08847bc307c08f456a4b26c0a4bc646ba84be
Vjeko <vjeko@E-Users-iMac.lan>
2018-03-15 15:35:12 +0100

    month names long form

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -22,7 +22,10 @@ export function getToday () {
   const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
   const dayOfWeek = weekday[today.getDay()]
   const dd = today.getDate()
-  const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const monthList = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ]
   const mm = monthList[today.getMonth() + 1]
   return (
     `${dayOfWeek} ${dd} ${mm}`
