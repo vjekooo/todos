853a4bbaa3c2a9ce8873e85e301456e7dac0e602
vjekooo <vjeko.pavicin@gmail.com>
2018-03-27 20:45:09 +0200

    gmail fotka

--- src/js/components/SignIn.jsx
+++ src/js/components/SignIn.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { auth, googleAuthProvider } from '../database'
-import user from '../../assets/images/user-50.png'
+import user from '../../assets/images/user-48.png'
 
 const SignIn = () => {
   return (
